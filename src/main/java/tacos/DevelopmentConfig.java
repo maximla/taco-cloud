6af5069af1a3e14c5de9f9135db191c7a121aa0b
Maks <maxim.ladutko@mail.ru>
2022-05-07 10:45:41 +0300

    command line runner

---
+++ src/main/java/tacos/DevelopmentConfig.java
@@ -0,0 +1,24 @@
+package tacos;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import tacos.data.IngredientRepository;
+import tacos.data.UserRepository;
+import tacos.security.PasswordEncoderHolder;
+
+@Profile({"home", "!work"})
+@Configuration
+public class DevelopmentConfig {
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository repo,
+                                        UserRepository userRepo,
+                                        PasswordEncoderHolder passwordEncoderHolder) {
+        /**
+         * Initialization of some beans directly for specific application config profile from the application.yml.
+         */
+        return null;
+    }
+}
