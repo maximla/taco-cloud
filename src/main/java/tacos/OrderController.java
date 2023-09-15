9e51b1db6950eee23a776cb81b978b39102a1968
ladutsko <ladutsko@psoft.by>
2023-09-15 13:13:49 +0300

    jdbc

--- src/main/java/tacos/OrderController.java
+++ src/main/java/tacos/OrderController.java
@@ -11,12 +11,18 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import tacos.data.OrderRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+
+    private OrderRepository orderRepo;
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
 
     @GetMapping("/current")
     public String orderForm(@ModelAttribute TacoOrder tacoOrder) {
@@ -28,7 +34,7 @@ public class OrderController {
         if (errors.hasErrors()) {
             return "orderForm";
         }
-        log.info("Order submitted: {}", order);
+        orderRepo.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
     }
